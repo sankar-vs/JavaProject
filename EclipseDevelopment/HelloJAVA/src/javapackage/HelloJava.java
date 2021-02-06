bbd973a960a8745d4a01c7aaaba0774aa926169e
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-02-06 22:13:17 +0530

    [Sankar]ADDED:all files to remote repo

---
+++ EclipseDevelopment/HelloJAVA/src/javapackage/HelloJava.java
@@ -0,0 +1,7 @@
+package javapackage;
+
+public class HelloJava {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
---
+++ EclipseDevelopment/JavaPractice/src/test/java/bridglabz/javapractice/AppTest.java
@@ -0,0 +1,20 @@
+package bridglabz.javapractice;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
